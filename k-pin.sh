9c1127dde34934b4da2cd70389de785c665e035b
Tian Yunxiao <53936026+hionwi@users.noreply.github.com>
2026-01-29 00:37:18 +0800

    Remove screenshot capture using Spectacle
    Remove Spectacle command for screenshot capture.

--- k-pin.sh
+++ k-pin.sh
@@ -3,9 +3,6 @@ TEMP_DIR="/tmp/spectacle_pins"
 mkdir -p "$TEMP_DIR"
 FILE_PATH="$TEMP_DIR/pin_tmp.png"
 
-# 使用 Spectacle 截取区域
-spectacle -rbn -o "$FILE_PATH"
-
 if [ -f "$FILE_PATH" ]; then
     # 以后台模式运行你写的看图程序
     ~/Scripts/k-view "$FILE_PATH" &
